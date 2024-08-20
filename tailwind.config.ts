08201e81d1912aa3eb4092efca62eaddcbdd4c91
filipe.burgonovo <burgodev@gmail.com>
2024-08-19 23:33:13 -0300

    style: Add  Tailwind Primary Color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#E50914",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
