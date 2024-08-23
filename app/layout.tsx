980d443d0ea9df70f4355f2da6145776ab1d25e7
filipe.burgonovo <burgodev@gmail.com>
2024-08-23 11:01:15 -0300

    feat: Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "../src/globals.css";
+import { Header } from "@/src/components/atomic/Header";
 
 export default function RootLayout({
   children,
@@ -13,22 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <html lang="en">
+      <ClerkProvider>
         <body>
-          <header className="flex justify-end p-6 m-auto max-w-[1560px] ">
-            <SignedOut>
-              <div className="bg-[#E50914] py-3 px-5 rounded-[6px] font-semibold">
-                <SignInButton />
-              </div>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+          <Header />
           <main>{children}</main>
         </body>
-      </html>
-    </ClerkProvider>
+      </ClerkProvider>
+    </html>
   );
 }
