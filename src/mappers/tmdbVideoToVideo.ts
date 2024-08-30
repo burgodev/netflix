2163e8a76759c6aff3612b43dcc92b4a76cc5985
filipe.burgonovo <burgodev@gmail.com>
2024-08-30 11:25:35 -0300

    chore: tmdbVideoToVideo mapper

---
+++ src/mappers/tmdbVideoToVideo.ts
@@ -0,0 +1,14 @@
+import { TmdbVideo } from "../types/api";
+
+// The Movie Database API to Video Mapper
+export const tmdbVideoToVideo = (tmdbVideo: TmdbVideo[]) =>
+  tmdbVideo.map((video) => ({
+    id: video.id,
+    title: video.title,
+    overview: video.overview,
+    mediaType: video.media_type,
+    backgroundImagePath: video.backdrop_path,
+    imagePath: video.poster_path,
+    releaseData: video.release_date,
+    rating: video.vote_average,
+  }));
