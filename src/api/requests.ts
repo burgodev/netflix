68b9ac7b60e958dab948f46233635df95f92735b
filipe.burgonovo <burgodev@gmail.com>
2024-08-30 11:24:54 -0300

    chore: The Movie Database requests

---
+++ src/api/requests.ts
@@ -0,0 +1,15 @@
+// add to env file
+const API_KEY = "9fb51088098dac7898059d3a83418853";
+
+const requests = {
+  fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorroMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumantaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
