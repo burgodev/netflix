c0872f936c0daa93cd1d87ffb9a47b6f2ba09002
filipe.burgonovo <burgodev@gmail.com>
2024-08-31 21:10:05 -0300

    chore: Rename Movie to Media

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { Banner } from "./Banner";
 export { Header } from "./Header";
-export { Movie } from "./Movie";
+export { Media } from "./Media";
 export { VideoList } from "./VideoList";
 export { Navigation } from "./Navigation";
 export * from "./atomic";
