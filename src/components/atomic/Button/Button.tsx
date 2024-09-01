fb002efee7ae2dd19da710169417f25c66a39ecd
filipe.burgonovo <filipe.burgonovo@ecoportal.co.nz>
2024-08-31 21:03:07 -0300

    fix: button semantic

--- src/components/atomic/Button/Button.tsx
+++ src/components/atomic/Button/Button.tsx
@@ -8,14 +8,14 @@ type StackProps = {
 
 const Button = ({ children, className }: StackProps) => {
   return (
-    <div
+    <button
       className={cn(
-        "cursor-pointer text-white outline-none border-none font-bold rounded-sm px-8 py-2 bg-gray-700 bg-opacity-50 hover:text-black hover:bg-gray-300 transition-all duration-200",
+        "cursor-pointer text-white outline-none border-none font-bold rounded-sm px-8 py-2 bg-gray-700 bg-opacity-50 hover:text-black hover:bg-gray-300 transition-all duration-200 h-[4vh]",
         className
       )}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
