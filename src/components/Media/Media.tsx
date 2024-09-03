78072e898693e5ad7738a7476146677b0b57eaa9
filipe.burgonovo <burgodev@gmail.com>
2024-09-02 21:52:09 -0300

    feat: Media image

--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -1,5 +1,30 @@
-const Media = () => {
-  return <div></div>;
+import { Video } from "@/src/types/api";
+import { Typography } from "../atomic";
+import { FC } from "react";
+import Image from "next/image";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+type VideoProps = {
+  video: Video;
+};
+// TODO: Video
+const Media: FC<VideoProps> = ({ video }) => {
+  console.log("video", video);
+  return (
+    <div className="relative min-w-[10vw] min-h-[10vw] flex-1">
+      <Image
+        objectFit="cover"
+        layout="fill"
+        alt={video.title}
+        src={`${base_url}${
+          // isLargeRow ? movie.poster_path : movie.backdrop_path
+          video.backgroundImage
+        }`}
+      />
+      {/* <Typography variant={"h1"}>{video.title}</Typography> */}
+    </div>
+  );
 };
 
 export default Media;
