db7da7c5945d194fc71b0c185e1ee6fea9e7e55f
filipe.burgonovo <burgodev@gmail.com>
2024-09-03 23:21:02 -0300

    fix: Page component tree

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,15 @@
 import { Banner } from "@/src/components/Banner";
+import { VideoContentWrapper } from "@/src/components/VideoContentWrapper";
 import { VideoList } from "@/src/components/VideoList";
-
 import { auth } from "@clerk/nextjs/server";
 
 export default function Home() {
   auth().protect();
+
   return (
     <div className="w-[100%] pl-[2.5vw]">
       <Banner />
-      <VideoList />
+      <VideoContentWrapper />
     </div>
   );
 }
