41b05decaab9f17be6a803c8caaa595ea1ce7217
filipe.burgonovo <burgodev@gmail.com>
2024-09-03 22:07:14 -0300

    style: Button component

--- src/components/atomic/Button/Button.tsx
+++ src/components/atomic/Button/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({ children, className }: StackProps) => {
   return (
     <button
       className={cn(
-        "cursor-pointer text-white outline-none border-none font-bold rounded-sm px-8 py-2 bg-gray-700 bg-opacity-50 hover:text-black hover:bg-gray-300 transition-all duration-200 h-[4vh]",
+        "cursor-pointer text-white outline-none border-none font-bold rounded-sm px-8 py-2 bg-gray-700 bg-opacity-50 hover:text-black hover:bg-gray-300 transition-all duration-200 min-h-[4vh]",
         className
       )}
     >
