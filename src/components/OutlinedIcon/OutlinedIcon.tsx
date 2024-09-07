b3c89fe2df5090859c5a4ef8e5253c5222826d15
filipe.burgonovo <burgodev@gmail.com>
2024-09-07 13:19:46 -0300

    style: OutlinedIcon hover

--- src/components/OutlinedIcon/OutlinedIcon.tsx
+++ src/components/OutlinedIcon/OutlinedIcon.tsx
@@ -6,7 +6,9 @@ type OutlinedIconProps = {
 
 const OutlinedIcon: FC<OutlinedIconProps> = ({ children }) => {
   return (
-    <div className="rounded-[50%] border border-gray-300 p-2">{children}</div>
+    <div className="rounded-[50%] border transition-all duration-300 border-[#2a2a2a] bg-[#FFFFFF1A] hover:border-white p-1.5">
+      {children}
+    </div>
   );
 };
 export default OutlinedIcon;
