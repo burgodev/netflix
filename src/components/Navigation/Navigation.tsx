13d20ae911d2350ac30504c91c6a7c1dc0b84982
filipe.burgonovo <burgodev@gmail.com>
2024-09-10 21:49:57 -0300

    style: Navigation typography

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import { Stack } from "../atomic/Stack";
+
 import { cn } from "@/src/utils/cn";
-import { Typography } from "../atomic";
-import { useRouter } from "next/router";
+
 import React from "react";
 import { usePathname } from "next/navigation";
 
@@ -14,52 +13,48 @@ const Navigation = () => {
   console.log("pathname", pathname);
 
   return (
-    <Stack className="align-center">
-      <ul className="flex gap-8 align-center">
-        <li>
-          <Link href="/home">
-            <Typography
-              variant="body"
-              className={cn(
-                pathname === "/home" ? "font-medium" : "text-[#e5e5e5]" // TODO: theme
-              )}
-            >
-              Home
-            </Typography>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/tv-shows"
-            className={cn(
-              pathname === "/tv-shows" ? "font-medium" : "text-[#e5e5e5]"
-            )}
-          >
-            Tv Shows
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/movies"
-            className={cn(
-              pathname === "/movies" ? "font-medium" : "text-[#e5e5e5]"
-            )}
-          >
-            Movies
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/my-list"
-            className={cn(
-              pathname === "/my-list" ? "font-medium" : "text-[#e5e5e5]"
-            )}
-          >
-            My List
-          </Link>
-        </li>
-      </ul>
-    </Stack>
+    <ul className="flex gap-8 align-center h-fit">
+      <li className="h-fit">
+        <Link
+          href="/home"
+          className={cn(
+            pathname === "/home" ? "font-medium" : "text-[#e5e5e5]" // TODO: theme
+          )}
+        >
+          Home
+        </Link>
+      </li>
+      <li className="h-fit">
+        <Link
+          href="/tv-shows"
+          className={cn(
+            pathname === "/tv-shows" ? "font-medium" : "text-[#e5e5e5]"
+          )}
+        >
+          Tv Shows
+        </Link>
+      </li>
+      <li className="h-fit">
+        <Link
+          href="/movies"
+          className={cn(
+            pathname === "/movies" ? "font-medium" : "text-[#e5e5e5]"
+          )}
+        >
+          Movies
+        </Link>
+      </li>
+      <li className="h-fit">
+        <Link
+          href="/my-list"
+          className={cn(
+            pathname === "/my-list" ? "font-medium" : "text-[#e5e5e5]"
+          )}
+        >
+          My List
+        </Link>
+      </li>
+    </ul>
   );
 };
 
