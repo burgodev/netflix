0e8cc68cad532dfbc11540db61bfc8657e1d77ba
filipe.burgonovo <burgodev@gmail.com>
2024-09-14 19:27:11 -0300

    style: Banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -28,7 +28,7 @@ const Banner = () => {
         className="object-contain"
       />
       <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent h-3/4 bottom-0 mt-auto"></div>
-      <div className="relative flex flex-col pt-36 h-48 gap-4 sm:w-[35.5vw] xl:w-[28.75vw] pl-[2.5vw]">
+      <div className="relative flex flex-col pt-36 h-48 gap-4 sm:w-[75vw] md:w-[50vw] xl:w-[35vw] pl-[2.5vw]">
         <Typography variant="body">{overview}</Typography>
 
         <Stack className="flex-row gap-3">
