70004cc423dac0bc8eeeb2ef0d9d9f4e7d98c06a
filipe.burgonovo <burgodev@gmail.com>
2024-09-17 22:27:52 -0300

    fix: Update banner to server side render

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,34 +1,30 @@
-"use client";
-
 import Image from "next/image";
 import { Typography } from "../atomic/Typography";
 import { Button, Stack } from "../atomic";
-import { useVideoQuery } from "@/src/hooks";
-import requests from "@/src/api/requests";
-import { base_url } from "../Video/Video";
+import { Video } from "@/src/types/api";
 
-// TODO: make this component dummy
-const Banner = () => {
-  const { videos, loading } = useVideoQuery({
-    fetchUrl: requests.fetchTopRated,
-  });
+type BannerProps = {
+  videos: Video[];
+};
 
-  if (!videos.length) return null;
+const randomIndex = Math.floor(Math.random() * 20);
 
-  const { backgroundImage, overview } = videos[Math.floor(Math.random() * 20)];
+const Banner = ({ videos }: BannerProps) => {
+  const { backgroundImage, overview } = videos[randomIndex];
 
   return (
-    <Stack className="relative w-screen h-[60vh] lg:h-[50vw] xl:h-[45vw] xxl:h-[40vw] justify-center">
+    <Stack className="relative w-screen h-[50vh] md:h-[45vw] justify-center">
       <Image
-        src={`${base_url}${backgroundImage}`}
+        src={backgroundImage}
         layout="fill"
         objectFit="cover"
         alt="Netflix Banner"
         objectPosition="top"
         className="object-contain"
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent h-3/4 bottom-0 mt-auto"></div>
-      <div className="relative flex flex-col pt-36 h-48 gap-4 sm:w-[75vw] md:w-[50vw] xl:w-[35vw] pl-[2.5vw]">
+      {/* Image overlay */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent h-[65%] mt-auto" />
+      <div className="relative flex flex-col pt-36 h-48 gap-4 w-[35.5vw] xl:w-[28.75vw] pl-[2.5vw]">
         <Typography variant="body">{overview}</Typography>
 
         <Stack className="flex-row gap-3">
