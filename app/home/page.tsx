2bb1cfc37451c853b8e5ce43d13b15eb4fb40e2d
filipe.burgonovo <burgodev@gmail.com>
2024-09-17 22:26:57 -0300

    fix: fetch videos on home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,31 @@
+import requests from "@/src/api/requests";
 import { Banner } from "@/src/components/Banner";
 import { VideoContentWrapper } from "@/src/components/VideoContentWrapper";
+import { tmdbVideoToVideo } from "@/src/mappers/tmdbVideoToVideo";
+import { Video } from "@/src/types/api";
 import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+// TODO: move this from here
+const fetchVideos = async (): Promise<Video[]> => {
+  const fetchUrl = requests.fetchTopRated;
+  const response = await fetch(fetchUrl);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data: ${response.status}`);
+  }
+  const json = await response.json();
+  return tmdbVideoToVideo(json.results);
+};
+
+const Home = async () => {
+  const videos = await fetchVideos();
   auth().protect();
 
   return (
     <>
-      <Banner />
+      <Banner videos={videos} />
       <VideoContentWrapper />
     </>
   );
-}
+};
+
+export default Home;
