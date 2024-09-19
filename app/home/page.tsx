b7cb211d75b405e3ba87dc4e7fbb14eb4677fe23
filipe.burgonovo <burgodev@gmail.com>
2024-09-18 21:40:04 -0300

    fix: Update page fetchVideos imports

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,23 +1,11 @@
+import { fetchVideos } from "@/src/api/fetchVideos";
 import requests from "@/src/api/requests";
 import { Banner } from "@/src/components/Banner";
 import { VideoContentWrapper } from "@/src/components/VideoContentWrapper";
-import { tmdbVideoToVideo } from "@/src/mappers/tmdbVideoToVideo";
-import { Video } from "@/src/types/api";
 import { auth } from "@clerk/nextjs/server";
 
-// TODO: move this from here
-const fetchVideos = async (): Promise<Video[]> => {
-  const fetchUrl = requests.fetchTopRated;
-  const response = await fetch(fetchUrl);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch data: ${response.status}`);
-  }
-  const json = await response.json();
-  return tmdbVideoToVideo(json.results);
-};
-
 const Home = async () => {
-  const videos = await fetchVideos();
+  const videos = await fetchVideos({ url: requests.fetchTopRated });
   auth().protect();
 
   return (
