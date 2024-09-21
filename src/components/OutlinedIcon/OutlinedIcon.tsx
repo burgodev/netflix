1b28d270532b02ded592a352772daa0511c2a1c3
filipe.burgonovo <burgodev@gmail.com>
2024-09-21 14:45:49 -0300

    style: OutlinedIcon

--- src/components/OutlinedIcon/OutlinedIcon.tsx
+++ src/components/OutlinedIcon/OutlinedIcon.tsx
@@ -1,12 +1,19 @@
+import { cn } from "@/src/utils/cn";
 import { FC } from "react";
 
 type OutlinedIconProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-const OutlinedIcon: FC<OutlinedIconProps> = ({ children }) => {
+const OutlinedIcon: FC<OutlinedIconProps> = ({ children, className }) => {
   return (
-    <div className="rounded-[50%] border transition-all duration-300 border-[#2a2a2a] bg-[#FFFFFF1A] hover:border-white p-1.5">
+    <div
+      className={cn(
+        "rounded-[50%] border transition-all duration-300 border-[#2a2a2a] bg-[#FFFFFF1A] hover:border-white p-1.5",
+        className
+      )}
+    >
       {children}
     </div>
   );
