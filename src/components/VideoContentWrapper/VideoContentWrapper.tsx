215f0ffdaa287d0fc28c711404abee9418be79b1
filipe.burgonovo <burgodev@gmail.com>
2024-09-20 23:25:37 -0300

    style: Video Wrapper padding on mobile

--- src/components/VideoContentWrapper/VideoContentWrapper.tsx
+++ src/components/VideoContentWrapper/VideoContentWrapper.tsx
@@ -7,7 +7,7 @@ type VideoCategoryProps = {};
 
 const VideoContentWrapper: FC<VideoCategoryProps> = () => {
   return (
-    <Stack className="pl-[2.5vw] w-screen gap-[5vh]">
+    <Stack className="pl-[16px] sm:pl-[2.5vw] w-screen gap-[5vh]">
       {VIDEO_CATEGORY_MOCK.map(({ id, title, fetchUrl }) => (
         <VideoCategory key={id} title={title} fetchUrl={fetchUrl} />
       ))}
