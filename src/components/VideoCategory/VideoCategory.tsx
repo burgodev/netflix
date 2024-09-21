b8b7f4717500754cdc954d075613a0ca5e22eef5
filipe.burgonovo <burgodev@gmail.com>
2024-09-21 16:30:55 -0300

    fix: VideoCategory server side render

--- src/components/VideoCategory/VideoCategory.tsx
+++ src/components/VideoCategory/VideoCategory.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import { FC } from "react";
 import { Stack, Typography } from "../atomic";
 import { VideoList } from "../VideoList";
-import { useVideoQuery } from "@/src/hooks";
+import { fetchVideos } from "@/src/api/fetchVideos";
 
 type VideoCategoryProps = {
   title: string;
   fetchUrl: string;
 };
 
-const VideoCategory: FC<VideoCategoryProps> = ({ title, fetchUrl }) => {
-  const { videos, loading } = useVideoQuery({ fetchUrl });
+const VideoCategory: FC<VideoCategoryProps> = async ({ title, fetchUrl }) => {
+  const videos = await fetchVideos({ url: fetchUrl });
 
   return (
     <Stack className="gap-2 min-h-[25vh]">
