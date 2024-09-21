01b90872eeb079e0fb751bb43e001ab3a56111ee
filipe.burgonovo <burgodev@gmail.com>
2024-09-21 16:46:04 -0300

    fix: Add default Next middleware

--- middleware.ts
+++ middleware.ts
@@ -10,3 +10,14 @@
 //     "/(api|trpc)(.*)",
 //   ],
 // };
+
+import { NextResponse, NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/home", request.url));
+}
+
+export const config = {
+  matcher: "/about/:path*",
+};
