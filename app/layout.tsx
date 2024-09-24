5e61df253e9569513d9ba0fbb11842c885bcf6db
filipe.burgonovo <burgodev@gmail.com>
2024-09-23 21:42:40 -0300

    feat: Display footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "tailwindcss/tailwind.css";
 // import { ClerkProvider } from "@clerk/nextjs";
 import "../src/globals.css";
 import { Header } from "../src/components/Header";
+import { Footer } from "@/src/components/Footer";
 
 export default function RootLayout({
   children,
@@ -14,9 +15,10 @@ export default function RootLayout({
       {/* <ClerkProvider> */}
       <body>
         <Header />
-        <main className="min-h-[calc(100vh-85px)] flex flex-col items-center overflow-hidden">
+        <main className="min-h-[calc(100vh-85px)] flex flex-col items-center overflow-hidden pb-[10vh]">
           {children}
         </main>
+        <Footer />
       </body>
       {/* </ClerkProvider> */}
     </html>
