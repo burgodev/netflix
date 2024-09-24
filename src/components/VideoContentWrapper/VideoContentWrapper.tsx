1342af6951cc03ba0f3eb3d51970a4c4ac81dfa4
filipe.burgonovo <burgodev@gmail.com>
2024-09-24 19:44:14 -0300

    style: Add padding bottom on video content wrapper

--- src/components/VideoContentWrapper/VideoContentWrapper.tsx
+++ src/components/VideoContentWrapper/VideoContentWrapper.tsx
@@ -7,7 +7,7 @@ type VideoCategoryProps = {};
 
 const VideoContentWrapper: FC<VideoCategoryProps> = () => {
   return (
-    <Stack className="pl-[16px] sm:pl-[2.5vw] pt-[2vh] w-screen gap-[5vh]">
+    <Stack className="pl-[16px] sm:pl-[2.5vw] pt-[2vh] w-screen gap-[5vh] pb-[25vh]">
       {VIDEO_CATEGORY_MOCK.map(({ id, title, fetchUrl }) => (
         <VideoCategory key={id} title={title} fetchUrl={fetchUrl} />
       ))}
