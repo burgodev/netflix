6df00da5a5f9b9b6dfbf81829ecda6ceac2379b1
filipe.burgonovo <burgodev@gmail.com>
2024-09-23 21:43:09 -0300

    style: Videoinfo icon size

--- src/components/Video/components/VideoInfo/VideoInfo.tsx
+++ src/components/Video/components/VideoInfo/VideoInfo.tsx
@@ -17,20 +17,20 @@ type VideoInfoProps = Pick<Video, "duration" | "genres"> & {
 const VideoInfo: FC<VideoInfoProps> = ({ duration, genres, display }) => {
   return (
     <Stack
-      className={`bg-[#141414] transition-opacity duration-300 p-2 sm:p-3 lg:p-4 gap-1 md:gap-2 lg:gap-3 border-radius rounded-[6px]${
+      className={`bg-black transition-opacity duration-300 p-2 sm:p-3 lg:p-4 gap-1 md:gap-2 lg:gap-3 border-radius rounded-[6px]${
         display ? "opacity-100" : "opacity-0"
       }`}
     >
       <Stack className="flex-row justify-between">
         <Stack className="flex-row gap-1 lg:gap-2">
           <OutlinedIcon>
-            <PlayIcon className="h-[2vw] w-[2vw] md:h-[1vw] md:w-[1vw] xl:h-[0.75vw] xl:w-[0.75vw] text-white" />
+            <PlayIcon className="h-[2vw] w-[2vw] md:h-[1vw] md:w-[1vw] xl:h-[0.9vw] xl:w-[0.9vw] text-white" />
           </OutlinedIcon>
           <OutlinedIcon>
-            <PlusIcon className="h-[2vw] w-[2vw] md:h-[1vw] md:w-[1vw] xl:h-[0.75vw] xl:w-[0.75vw] text-white" />
+            <PlusIcon className="h-[2vw] w-[2vw] md:h-[1vw] md:w-[1vw] xl:h-[0.9vw] xl:w-[0.9vw] text-white" />
           </OutlinedIcon>
           <OutlinedIcon>
-            <HandThumbUpIcon className="h-[2vw] w-[2vw] md:h-[1vw] md:w-[1vw] xl:h-[0.75vw] xl:w-[0.75vw] text-white" />
+            <HandThumbUpIcon className="h-[2vw] w-[2vw] md:h-[1vw] md:w-[1vw] xl:h-[0.9vw] xl:w-[0.9vw] text-white" />
           </OutlinedIcon>
         </Stack>
 
