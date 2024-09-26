807749a8e3ed4c60391e3a3ab6a0f34e05fa701f
filipe.burgonovo <burgodev@gmail.com>
2024-09-26 18:57:17 -0300

    feat: VideoList navigation start

--- src/components/VideoList/VideoList.tsx
+++ src/components/VideoList/VideoList.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { Stack } from "../atomic";
 import { Video } from "../Video";
 import { Video as VideoType } from "@/src/types/api";
@@ -8,16 +10,45 @@ type VideoListProps = {
 };
 
 const VideoList: FC<VideoListProps> = ({ videos }) => {
+  const [translateX, setTranslateX] = useState(0);
+
+  const handleScrollLeft = () => {
+    setTranslateX((prev) => Math.min(prev + 95, 0)); // Adjust the value as needed
+  };
+
+  const handleScrollRight = () => {
+    setTranslateX((prev) => prev - 95); // Adjust the value as needed
+  };
+
+  console.log("translateX", translateX);
+
   return (
-    <Stack className="overflow-visible flex-row gap-2 cursor-pointer">
-      {videos.map((video, i) => (
-        <Video
-          video={video}
-          hoverAlign={i === 0 ? "left" : "center"}
-          key={video.id}
-        />
-      ))}
-    </Stack>
+    <div className="relative">
+      <button
+        onClick={handleScrollLeft}
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-40"
+      >
+        &lt;
+      </button>
+      <Stack
+        className="relative overflow-visible flex-row gap-2 cursor-pointer transition-transform duration-500 hover:z-20"
+        style={{ transform: `translateX(${translateX}vw)` }}
+      >
+        {videos.map((video, i) => (
+          <Video
+            video={video}
+            hoverAlign={i === 0 ? "left" : "center"}
+            key={video.id}
+          />
+        ))}
+      </Stack>
+      <button
+        onClick={handleScrollRight}
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-40"
+      >
+        &gt;
+      </button>
+    </div>
   );
 };
 
