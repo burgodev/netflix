5acc08a1f587e8921a281af117905e0d3cbdac6c
filipe.burgonovo <burgodev@gmail.com>
2024-09-28 20:16:32 -0300

    feat: Video display start

--- app/watch/[videoId]/page.tsx
+++ app/watch/[videoId]/page.tsx
@@ -1,19 +1,25 @@
 import { fetchVideos } from "@/src/api/fetchVideos";
 import requests from "@/src/api/requests";
+import { getVimeoVideos } from "@/src/api/vimeo";
 import { Stack } from "@/src/components";
 import { Banner } from "@/src/components/Banner";
 import { VideoContentWrapper } from "@/src/components/VideoContentWrapper";
+import { VideoDisplay } from "@/src/components/VideoDisplay";
 // import { auth } from "@clerk/nextjs/server";
 
 const randomIndex = Math.floor(Math.random() * 20);
 
 const Home = async () => {
   const videos = await fetchVideos({ url: requests.fetchTopRated });
+  // const vimeoVideos = await getVimeoVideos();
+
+  // console.log("vimeoVideos", vimeoVideos);
+
   // auth().protect();
 
   return (
     <Stack>
-      <h1>video</h1>
+      <VideoDisplay />
     </Stack>
   );
 };
