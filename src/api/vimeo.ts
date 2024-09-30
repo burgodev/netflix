a18a4becddb77580b715dfa6f68309e8fdda9bbb
filipe.burgonovo <burgodev@gmail.com>
2024-09-30 19:19:30 -0300

    feat: get/fetch vimeo access token

--- src/api/vimeo.ts
+++ src/api/vimeo.ts
@@ -2,7 +2,15 @@ const CLIENT_ID = "160a0f5414634953a5d79b7b9063c958c93aa8cd"; // TODO: env
 const CLIENT_SECRET =
   "8MLSNhYIonfJmkHtu1U5laZGGdkK/woO8Y+qvXznrLtYOjJd6yDo3e4ubUYITKEB4bEOK2UStGzo+/DhTFX8EYp2qXi/EgS63Z6QCm0pO2Z+d6xxrvTJ3g8yk5eKEB3U";
 
-export const getVimeoAccessToken = async () => {
+export const getVimeoAccessToken = () => {
+  const token = localStorage.getItem("access_token");
+
+  if (!token) return fetchVimeoAccessToken();
+
+  return token;
+};
+
+export const fetchVimeoAccessToken = async () => {
   const response = await fetch("https://api.vimeo.com/oauth/authorize/client", {
     method: "POST",
     headers: {
@@ -22,11 +30,7 @@ export const getVimeoAccessToken = async () => {
 };
 
 export const getVimeoVideos = async () => {
-  // const accessToken = await getVimeoAccessToken();
-
-  // console.log("accessToken", accessToken);
-
-  const token = localStorage.getItem("access_token");
+  const token = getVimeoAccessToken();
 
   const response = await fetch("https://api.vimeo.com/users/227519127/videos", {
     headers: {
@@ -35,7 +39,6 @@ export const getVimeoVideos = async () => {
   });
 
   // console.log("getVimeoVideos response", response);
-
   const data = await response.json();
 
   console.log("getVimeoVideos data", data);
@@ -47,7 +50,9 @@ export const getVimeoVideo = async ({ videoId }: { videoId: string }) => {
 
   // console.log("getVimeoVideo accessToken", accessToken);
 
-  const token = localStorage.getItem("access_token");
+  const token = getVimeoAccessToken();
+
+  console.log("token", token);
 
   const response = await fetch(`https://api.vimeo.com/videos/${videoId}`, {
     headers: {
