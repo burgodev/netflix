79a0a94dd30d8efc5d9d1a0016023dd4eba5fe96
filipe.burgonovo <burgodev@gmail.com>
2024-10-01 20:16:17 -0300

    feat: Get videos list from Vimeo API

--- src/components/VideoCategory/VideoCategory.tsx
+++ src/components/VideoCategory/VideoCategory.tsx
@@ -1,15 +1,31 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useEffect, useState } from "react";
 import { Stack, Typography } from "../atomic";
 import { VideoList } from "../VideoList";
 import { fetchVideos } from "@/src/api/fetchVideos";
+import { getVimeoVideos } from "@/src/api/vimeo";
 
 type VideoCategoryProps = {
   title: string;
   fetchUrl: string;
 };
 
-const VideoCategory: FC<VideoCategoryProps> = async ({ title, fetchUrl }) => {
-  const videos = await fetchVideos({ url: fetchUrl });
+const VideoCategory: FC<VideoCategoryProps> = ({ title, fetchUrl }) => {
+  // const videos = await fetchVideos({ url: fetchUrl });
+
+  const [videos, setVideos] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchVideo = async () => {
+      const videos = await getVimeoVideos();
+      setVideos(videos);
+    };
+
+    fetchVideo();
+  }, []);
+
+  console.log("videos", videos);
 
   return (
     <Stack className="gap-2 min-h-[25vh]">
