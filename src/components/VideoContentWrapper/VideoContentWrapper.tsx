4ba535395e724873aecd7a1a3dfe870eecf88bda
filipe.burgonovo <burgodev@gmail.com>
2024-10-07 22:26:23 -0300

    style: Video content wrapper

--- src/components/VideoContentWrapper/VideoContentWrapper.tsx
+++ src/components/VideoContentWrapper/VideoContentWrapper.tsx
@@ -7,10 +7,12 @@ type VideoCategoryProps = {};
 
 const VideoContentWrapper: FC<VideoCategoryProps> = () => {
   return (
-    <Stack className="pl-[16px] sm:pl-[2.5vw] pt-[2vh] w-screen gap-[5vh] pb-[25vh]">
-      {VIDEO_CATEGORY_MOCK.map(({ id, title, fetchUrl }) => (
-        <VideoCategory key={id} title={title} fetchUrl={fetchUrl} />
-      ))}
+    <Stack className="px-[16px] sm:px-[2.5vw] pt-[2vh] w-screen gap-[5vh] pb-[25vh]">
+      {VIDEO_CATEGORY_MOCK.filter((item, i) => i === 0).map(
+        ({ id, title, fetchUrl }) => (
+          <VideoCategory key={id} title={title} fetchUrl={fetchUrl} />
+        )
+      )}
     </Stack>
   );
 };
