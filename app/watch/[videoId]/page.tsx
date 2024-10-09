d532199b8d407fad45241de4effc076ca1b6c01a
filipe.burgonovo <burgodev@gmail.com>
2024-10-08 21:46:58 -0300

    cleanup: Remove get access token from watch page

--- app/watch/[videoId]/page.tsx
+++ app/watch/[videoId]/page.tsx
@@ -1,23 +1,14 @@
-"use client";
-
-import { fetchVimeoAccessToken, getVimeoAccessToken } from "@/src/api/vimeo";
+import { fetchVimeoAccessToken, getVimeoAccessToken, getVimeoVideo } from "@/src/api/vimeo";
 import { Stack } from "@/src/components";
 import { VideoDisplay } from "@/src/components/VideoDisplay";
 import { VideoPlaylist } from "@/src/components/VideoPlaylist";
 
-const Watch = () => {
-  const token = getVimeoAccessToken();
-
-  console.log("token", token);
-
-  if (token === "undefined" || !token) {
-    console.log("!token");
-    fetchVimeoAccessToken();
-  }
+const Watch = async () => {
+  const video = await getVimeoVideo({ videoId });
 
   return (
     // TODO: is this the best way of compensating for header height?
-    <Stack className="h-screen w-screen flex-row mt-[85px]"> 
+    <Stack className="h-screen w-screen flex-row mt-[85px]">
       <VideoDisplay />
       <VideoPlaylist />
     </Stack>
