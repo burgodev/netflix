5032a92e217e978eeba94c76fb68f8f7eda86440
filipe.burgonovo <burgodev@gmail.com>
2024-10-10 19:02:10 -0300

    fix: Remove filter from VideoContentWrapper

--- src/components/VideoContentWrapper/VideoContentWrapper.tsx
+++ src/components/VideoContentWrapper/VideoContentWrapper.tsx
@@ -2,17 +2,23 @@ import { FC } from "react";
 import { VideoCategory } from "../VideoCategory";
 import { VIDEO_CATEGORY_MOCK } from "@/src/api/mocks";
 import { Stack } from "../atomic";
+import { Video } from "@/src/types/api";
 
-type VideoCategoryProps = {};
+interface VideoCategoryProps {
+  videos: Video[];
+}
 
-const VideoContentWrapper: FC<VideoCategoryProps> = () => {
+const VideoContentWrapper: FC<VideoCategoryProps> = ({ videos }) => {
   return (
     <Stack className="px-[16px] sm:px-[2.5vw] pt-[2vh] w-screen gap-[5vh] pb-[25vh]">
-      {VIDEO_CATEGORY_MOCK.filter((item, i) => i === 0).map(
-        ({ id, title, fetchUrl }) => (
-          <VideoCategory key={id} title={title} fetchUrl={fetchUrl} />
-        )
-      )}
+      {VIDEO_CATEGORY_MOCK.map(({ id, title, fetchUrl }) => (
+        <VideoCategory
+          key={id}
+          title={title}
+          fetchUrl={fetchUrl}
+          videos={videos}
+        />
+      ))}
     </Stack>
   );
 };
