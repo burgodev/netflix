92409f3b191f432e0887b4bf8885b434fe6ffc15
filipe.burgonovo <burgodev@gmail.com>
2024-10-09 21:22:52 -0300

    fix: Get videoId param on watch page

--- app/watch/[videoId]/page.tsx
+++ app/watch/[videoId]/page.tsx
@@ -1,9 +1,22 @@
-import { fetchVimeoAccessToken, getVimeoAccessToken, getVimeoVideo } from "@/src/api/vimeo";
+import {
+  fetchVimeoAccessToken,
+  getVimeoAccessToken,
+  getVimeoVideo,
+} from "@/src/api/vimeo";
 import { Stack } from "@/src/components";
 import { VideoDisplay } from "@/src/components/VideoDisplay";
 import { VideoPlaylist } from "@/src/components/VideoPlaylist";
 
-const Watch = async () => {
+export type WatchProps = {
+  params: {
+    videoId: string;
+  };
+};
+
+
+
+const Watch = async ({ params }: WatchProps) => {
+  const { videoId } = params;
   const video = await getVimeoVideo({ videoId });
 
   return (
