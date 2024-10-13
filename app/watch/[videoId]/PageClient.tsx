eec0676354bbc45d565eacb2562f46f3512816bc
filipe.burgonovo <burgodev@gmail.com>
2024-10-13 19:55:42 -0300

    cleanup: Watch PageClient

--- app/watch/[videoId]/PageClient.tsx
+++ app/watch/[videoId]/PageClient.tsx
@@ -1,36 +1,21 @@
 "use client";
 
-import { Provider } from "react-redux";
-import { Stack, VideoList } from "@/src/components";
-import { Banner } from "@/src/components/Banner";
-import { VideoContentWrapper } from "@/src/components/VideoContentWrapper";
-import { AppState, initializeStore } from "@/src/state/store";
+import { Stack } from "@/src/components";
+
 import { Video } from "@/src/types/api";
 import { VideoDisplay } from "@/src/components/VideoDisplay";
 import { VideoPlaylist } from "@/src/components/VideoPlaylist";
 
 interface PageClientProps {
-  preloadedState: AppState;
   video: Video;
-  randomIndex: number;
-  accessToken: string;
 }
 
-const PageClient: React.FC<PageClientProps> = ({
-  preloadedState,
-  video,
-  randomIndex,
-  accessToken,
-}) => {
-  const reduxStore = initializeStore(preloadedState);
-
+const PageClient: React.FC<PageClientProps> = ({ video }) => {
   return (
-    <Provider store={reduxStore}>
-      <Stack className="h-screen w-screen flex-row mt-[85px]">
-        <VideoDisplay video={video} />
-        <VideoPlaylist />
-      </Stack>
-    </Provider>
+    <Stack className="h-screen w-screen flex-row mt-[85px]">
+      <VideoDisplay video={video} />
+      <VideoPlaylist />
+    </Stack>
   );
 };
 
