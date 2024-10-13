db56df3e53999abc1b02611e6433a7fa309bb7b2
filipe.burgonovo <burgodev@gmail.com>
2024-10-13 19:55:31 -0300

    cleanup: Watch page

--- app/watch/[videoId]/page.tsx
+++ app/watch/[videoId]/page.tsx
@@ -1,44 +1,22 @@
-import {
-  fetchVimeoAccessToken,
-  getVimeoAccessToken,
-  getVimeoVideo,
-  getVimeoVideos,
-} from "@/src/api/vimeo";
-import { Stack } from "@/src/components";
-import { VideoDisplay } from "@/src/components/VideoDisplay";
-import { VideoPlaylist } from "@/src/components/VideoPlaylist";
-import { initializeStore } from "@/src/state/store";
+import { getVimeoVideo } from "@/src/api/vimeo";
+
 import { cookies } from "next/headers";
 import PageClient from "./PageClient";
 
-export type WatchProps = {
+interface WatchProps {
   params: {
     videoId: string;
   };
-};
+}
 
 const Watch = async ({ params }: WatchProps) => {
   const cookieStore = cookies();
   const accessToken = cookieStore.get("accessToken")?.value || "";
   const { videoId } = params;
 
-  const preloadedState = { accessToken: { token: accessToken } };
-  const reduxStore = initializeStore(preloadedState);
-  const { getState } = reduxStore;
-
-  const videos = await getVimeoVideos(accessToken);
   const video = await getVimeoVideo({ token: accessToken, videoId });
 
-  return (
-    // TODO: is this the best way of compensating for header height?
-
-    <PageClient
-      preloadedState={getState()}
-      accessToken={accessToken}
-      videos={videos}
-      video={video}
-    />
-  );
+  return <PageClient video={video} />;
 };
 
 export default Watch;
