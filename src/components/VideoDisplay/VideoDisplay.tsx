f7053764fd92dac31cf498775bf64a45d23e7fb7
filipe.burgonovo <burgodev@gmail.com>
2024-10-16 22:18:07 -0300

    style: VideoDisplay container size

--- src/components/VideoDisplay/VideoDisplay.tsx
+++ src/components/VideoDisplay/VideoDisplay.tsx
@@ -6,10 +6,10 @@ interface PageClientProps {
 
 const VideoDisplay = ({ video }: PageClientProps) => {
   return (
-    <div className="min-w-[60%]">
+    <div className="min-w-[50%]">
       {video && video.player_embed_url && (
         <iframe
-          className="w-[1200px] h-[80vh] aspect-video"
+          className="w-[60vw] h-[80vh] aspect-video"
           src={video.player_embed_url}
           width="100%"
           height="50%"
