430d48ba689efbd32f1ee5de7ee611829ea4af9c
filipe.burgonovo <burgodev@gmail.com>
2024-10-16 22:18:21 -0300

    feat: Add Video component on VideoPlaylist

--- src/components/VideoPlaylist/VideoPlaylist.tsx
+++ src/components/VideoPlaylist/VideoPlaylist.tsx
@@ -1,15 +1,25 @@
-import { Video } from "@/src/types/api";
+import { Video as VideoInterface } from "@/src/types/api";
 import { Stack, Typography } from "../atomic";
+import Video from "../Video/Video";
 
 interface VideoPlaylistProps {
-  videos: Video[];
+  videos: VideoInterface[];
 }
 
 const VideoPlaylist: React.FC<VideoPlaylistProps> = ({ videos }) => {
+  console.log("videos adas", videos);
+
+  if (!videos) return null;
   return (
     <Stack className="gap-4 flex-1">
       {videos.map((video) => (
-        <Typography key={video.uri}>{video.name}</Typography>
+        <Stack key={video.uri} className="flex-row gap-4">
+          <Video
+            video={{ ...video, backgroundImage: video.pictures.base_link }}
+            isVertical
+          />
+          <Typography variant="span">{video.name}</Typography>
+        </Stack>
       ))}
     </Stack>
   );
