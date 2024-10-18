4500c09ac624d560633e2d46af24eb49db0871f4
filipe.burgonovo <burgodev@gmail.com>
2024-10-17 22:54:49 -0300

    cleanup: home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,8 @@
-import { fetchVideos } from "@/src/api/fetchVideos";
-import requests from "@/src/api/requests";
 import { initializeStore } from "@/src/state/store";
 
 import { cookies } from "next/headers";
 import HomeClient from "./HomeClient";
 import { getVimeoVideos } from "@/src/api/vimeoApi";
-
-const randomIndex = Math.floor(Math.random() * 20);
 
 const Home = async () => {
   const cookieStore = cookies();
@@ -18,14 +14,7 @@ const Home = async () => {
 
   const videos = await getVimeoVideos({ token: accessToken });
 
-  return (
-    <HomeClient
-      preloadedState={getState()}
-      accessToken={accessToken}
-      videos={videos}
-      randomIndex={randomIndex}
-    />
-  );
+  return <HomeClient preloadedState={getState()} videos={videos} />;
 };
 
 export default Home;
